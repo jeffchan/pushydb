807a9aeac9907f7330feb6dd73c3959410e9fc92
Jeff Chan <jeffchan@mit.edu>
2014-02-12 22:21:06 -0500

    lab2 part a

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -6,8 +6,8 @@ import "net/rpc"
 import "log"
 import "time"
 import "sync"
-import "fmt"
 import "os"
+import "strconv"
 
 type ViewServer struct {
   mu sync.Mutex
@@ -15,30 +15,94 @@ type ViewServer struct {
   dead bool
   me string
 
+  lastPing map[string]*LastPing
+  view View // the current view
+}
 
-  // Your declarations here.
+type LastPing struct {
+  Viewnum uint // last ping's viewnum
+  Timestamp time.Time // last ping's timestamp
+  Restart bool
 }
 
 //
 // server Ping RPC handler.
 //
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
 
-  // Your code here.
+  address := args.Me
+  _, ok := vs.lastPing[address]
+
+  var newPing LastPing
+  newPing.Viewnum = args.Viewnum
+  newPing.Timestamp = time.Now()
+
+  log.Println(address + " ping #" + strconv.Itoa(int(args.Viewnum)))
+  if ok {
+    if args.Viewnum == 0 && vs.view.Viewnum > 0 {
+      log.Println(" Signal restart")
+      newPing.Restart = true
+    } else {
+      newPing.Restart = false
+    }
+  } else {
+    newPing.Restart = false
+  }
+  vs.lastPing[address] = &newPing
+  reply.View = vs.view
 
   return nil
 }
 
-// 
+//
 // server Get() RPC handler.
 //
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
 
-  // Your code here.
+  reply.View = vs.view
 
   return nil
 }
 
+func (vs *ViewServer) isDead(address string) bool {
+  if address == "" {
+    return true
+  }
+
+  lastPing, ok := vs.lastPing[address]
+  if ok {
+    return time.Now().Sub(lastPing.Timestamp) > (DeadPings * PingInterval)
+  }
+  return true
+}
+
+func (vs *ViewServer) primaryEligible(address string) bool {
+  lastPing, ok := vs.lastPing[address]
+  if !ok {
+    return false
+  }
+
+  return !(vs.isDead(address) || lastPing.Restart)
+}
+
+func (vs *ViewServer) nextAvail() string {
+  for address,_ := range vs.lastPing {
+    if (!vs.isDead(address) &&
+        vs.view.Primary != address &&
+        vs.view.Backup != address) {
+      return address
+    }
+  }
+  return ""
+}
+
+func (vs *ViewServer) nextView(primary string, secondary string) {
+  vs.view = View{ vs.view.Viewnum+1, primary, secondary }
+}
 
 //
 // tick() is called once per PingInterval; it should notice
@@ -46,8 +110,48 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 // accordingly.
 //
 func (vs *ViewServer) tick() {
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
 
-  // Your code here.
+  primary := vs.view.Primary
+  backup := vs.view.Backup
+
+  // When the viewservice first starts, it should accept
+  // any server at all as the first primary
+  if vs.view.Viewnum == 0 {
+    avail := vs.nextAvail()
+    if avail != "" {
+      vs.nextView(avail, "")
+    }
+    return
+  }
+
+  lastPing, ok := vs.lastPing[primary]
+  if !ok { return }
+
+  // Special case if the primary restarted, must have new primary
+  if vs.lastPing[primary].Restart {
+    if vs.primaryEligible(backup) {
+      vs.nextView(backup, vs.nextAvail())
+    } else {
+      // screwed if backup is not eligble -> all data lost
+      vs.nextView("", "")
+    }
+    return
+  }
+
+  // Don't proceed to new view until the primary from current view
+  // sends an ACK that it is operating in current view
+  if lastPing.Viewnum < vs.view.Viewnum {
+    return
+  }
+
+  if !vs.primaryEligible(primary) {
+    // promote backup to primary
+    vs.nextView(backup, vs.nextAvail())
+  } else if vs.isDead(backup) {
+    vs.nextView(primary, vs.nextAvail())
+  }
 }
 
 //
@@ -63,7 +167,9 @@ func (vs *ViewServer) Kill() {
 func StartServer(me string) *ViewServer {
   vs := new(ViewServer)
   vs.me = me
-  // Your vs.* initializations here.
+  vs.dead = false
+  vs.lastPing = make(map[string]*LastPing)
+  vs.view = View{0, "", ""}
 
   // tell net/rpc about our RPC server and handlers.
   rpcs := rpc.NewServer()
@@ -91,7 +197,7 @@ func StartServer(me string) *ViewServer {
         conn.Close()
       }
       if err != nil && vs.dead == false {
-        fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
+        log.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
         vs.Kill()
       }
     }
