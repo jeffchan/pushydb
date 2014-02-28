aec8a50a29b80eb3609680127b3ce0975e821f75
Jeff Chan <jeffchan@mit.edu>
2014-02-28 14:55:29 -0500

    Don't fork

--- src/paxos/test.sh
+++ src/paxos/test.sh
@@ -4,5 +4,6 @@ trap "echo Exited!; exit;" SIGINT SIGTERM
 
 for i in $(seq 0 20);
 do
-	 go test $1 2>$i.error | egrep -v 'EOF|files|connection|broken|connected|wrong' > $i.log  &
+	 go test $1 2>${i}a.error | egrep -v 'EOF|files|connection|broken|connected|wrong' > ${i}a.log
+	 go test $1 2>${i}b.error | egrep -v 'EOF|files|connection|broken|connected|wrong' > ${i}b.log
 done
