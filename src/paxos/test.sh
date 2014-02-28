d3dded59c3421bc248e087dfcdd6326d2157b446
Jeff Chan <jeffchan@mit.edu>
2014-02-28 03:15:50 -0500

    errors to log

--- src/paxos/test.sh
+++ src/paxos/test.sh
@@ -4,5 +4,5 @@ trap "echo Exited!; exit;" SIGINT SIGTERM
 
 for i in $(seq 0 20);
 do
-	 go test $1 2>/dev/null | egrep -v 'EOF|files|connection|broken|connected' > $i.log &
+	 go test $1 2>$i.error | egrep -v 'EOF|files|connection|broken|connected|wrong' > $i.log  &
 done
