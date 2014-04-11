8ae5ada82de6d42b437773641a97b3727f9f73ad
Jeff Chan <jeffchan@mit.edu>
2014-04-11 17:08:05 -0400

    Add test script

---
+++ src/shardkv/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage: ./test.sh [-test.run TestUnreliable]
+
+trap "echo Exited!; exit;" SIGINT SIGTERM
+
+mkdir -p out
+
+for i in $(seq 0 9);
+do
+  echo "Running trial ${i}"
+	go test $1 2>out/${i}.error | egrep -v 'EOF|files|connection|broken|connected' > out/${i}.log &
+done
