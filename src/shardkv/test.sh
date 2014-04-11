973e03de86df5358ed4d06c06fbd34ba6b39fa60
Jeff Chan <jeffchan@mit.edu>
2014-04-11 17:08:59 -0400

    Update test script

--- src/shardkv/test.sh
+++ src/shardkv/test.sh
@@ -5,8 +5,8 @@ trap "echo Exited!; exit;" SIGINT SIGTERM
 
 mkdir -p out
 
-for i in $(seq 0 9);
+for i in $(seq 0 1000);
 do
   echo "Running trial ${i}"
-	go test $1 2>out/${i}.error | egrep -v 'EOF|files|connection|broken|connected' > out/${i}.log &
+	go test $1 2>out/${i}.error | egrep -v 'EOF|files|connection|broken|connected' > out/${i}.log
 done
