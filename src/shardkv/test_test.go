222e34fad373538fb3baa020d7dac87d7bbcd82b
Oliver Song <osong@mit.edu>
2014-05-05 16:30:32 -0400

    Persistence test

--- src/shardkv/test_test.go
+++ src/shardkv/test_test.go
@@ -407,3 +407,61 @@ func TestConcurrentUnreliable(t *testing.T) {
   doConcurrent(t, true)
   fmt.Printf("  ... Passed\n")
 }
+
+func TestPersistenceDiskOkay(t *testing.T) {
+  smh, gids, ha, sa, clean := setup("persistencegood", false)
+  defer clean()
+
+  fmt.Printf("Test: Server recovers after failure and no disk loss...\n")
+
+  mck := shardmaster.MakeClerk(smh)
+  mck.Join(gids[0], ha[0])
+
+  ck := MakeClerk(smh)
+  defer cleanupClerk(ck)
+
+  ck.Put("a", "x")
+  v := ck.PutHash("a", "b")
+  if v != "x" {
+    t.Fatalf("Puthash got wrong value")
+  }
+  ov := NextValue("x", "b")
+  if ck.Get("a") != ov {
+    t.Fatalf("Get got wrong value")
+  }
+
+  keys := make([]string, 10)
+  vals := make([]string, len(keys))
+  for i := 0; i < len(keys); i++ {
+    keys[i] = strconv.Itoa(rand.Int())
+    vals[i] = strconv.Itoa(rand.Int())
+    ck.Put(keys[i], vals[i])
+  }
+
+  // are keys still there after kill and restart?
+  randomGroup := rand.Intn(length(sa))
+  randomSKV := rand.Intn(length(sa[randomGroup]))
+  mb := sa[randomGroup][randomSKV].mb
+  sa[randomGroup][randomSKV].kill()
+  time.Sleep(1 * time.Second)
+  sa[randomGroup][randomSKV] = StartServer(gids[randomGroup], smh, ha[randomGroup], randomSKV, mb)
+  time.Sleep(2 * time.Second)
+  for i := 0; i < len(keys); i++ {
+    v := sa[randomGroup][randomSKV].table[keys[i]]
+    if v != vals[i] {
+      t.Fatalf("killed and restarted; wrong value; g=%v k=%v wanted=%v got=%v",
+        g, keys[i], vals[i], v)
+    }
+    // vals[i] = strconv.Itoa(rand.Int())
+    // ck.Put(keys[i], vals[i])
+  }
+
+  fmt.Printf("  ... Passed\n")
+}
+
+func TestPersistenceDiskLoss(t *testing.T) {
+  smh, gids, ha, _, clean := setup("persistencebad", false)
+  defer clean()
+
+  fmt.Printf("Test: Server recovers after failure and total disk loss...\n")
+}
